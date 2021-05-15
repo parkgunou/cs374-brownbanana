46c34dd7214dc106909bb8497f861eadc4291c98
raphaelshim <raphaelshim@kaist.ac.kr>
2021-05-15 18:56:42 +0900

    Image list added

---
+++ src/components/ImageListItem.js
@@ -0,0 +1,21 @@
+import { Image } from 'antd';
+import React from 'react';
+import '../css/StyleImage.css';
+
+
+export default class ImageListItem extends React.Component {
+
+    render() {
+        return ( <
+            div id = "imagelist" >
+            <
+            Image className = "style-images"
+            width = { 200 }
+            src = { this.props.image_src }
+            /> <
+            div id = "imageDescription" > { this.props.style } <
+            /div>   < /
+            div >
+        );
+    }
+}
