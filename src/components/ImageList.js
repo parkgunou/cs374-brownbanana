8fec5a3f9ac7791bf0c2393b9962127c272e39af
raphaelshim <raphaelshim@kaist.ac.kr>
2021-05-15 18:54:18 +0900

    Image list

---
+++ src/components/ImageList.js
@@ -0,0 +1,61 @@
+import { List } from 'antd';
+import React from 'react';
+import ImageListItem from './ImageListItem';
+import { getFirebaseDB } from '../Firebase';
+
+const imageDes = [{
+        "style": "Swallow perm",
+        "description": "I am a girl",
+        "image_url": "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+    },
+    {
+        "style": "Ash perm",
+        "description": "I am a girl",
+        "image_url": "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+    },
+    {
+        "style": "Man haircut",
+        "description": "I am a girl",
+        "image_url": "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+    },
+    {
+        "style": "Woman haircut",
+        "description": "I am a girl",
+        "image_url": "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+    }
+]
+
+export default class ImageList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            imageList: []
+        }
+    }
+
+    componentDidMount() {
+        getFirebaseDB().once('value').then((snapshot) => {
+            this.setState({
+                imageList: imageDes
+            })
+        })
+    }
+
+    render() {
+        return ( <
+            List grid = {
+                { gutter: 16, column: 3 }
+            }
+            dataSource = { this.state.imageList }
+            renderItem = {
+                item => ( <
+                    ImageListItem style = { item.style }
+                    description = { item.description }
+                    image_src = { item.image_url }
+                    />
+                )
+            }
+            />
+        )
+    }
+};
--- src/layouts/ProfileView.js
+++ src/layouts/ProfileView.js
@@ -3,31 +3,46 @@ import { Layout, Tabs } from 'antd';
 import MenuList from '../components/MenuList';
 import ProfileBio from '../components/ProfileBio';
 import ReviewList from '../components/ReviewList';
+import ImageList from '../components/ImageList';
 import { HeaderWithSearch } from '../components/Header';
 
 const { Content } = Layout;
 const { TabPane } = Tabs;
 
 function ProfileView() {
-  return (
-    <Layout className="layout">
-      <HeaderWithSearch></HeaderWithSearch>
-      <ProfileBio />
-      <Content style={{ paddingLeft: '20%', paddingRight: '20%' }}>
-        <Tabs defaultActiveKey="1">
-          <TabPane tab="Styles" key="1">
-
-          </TabPane>
-          <TabPane tab="Reviews" key="2">
-            <ReviewList />
-          </TabPane>
-          <TabPane tab="Menu" key="3">
-            <MenuList />
-          </TabPane>
-        </Tabs>
-      </Content>
-    </Layout>
-  );
+    return ( <
+        Layout className = "layout" >
+        <
+        HeaderWithSearch > < /HeaderWithSearch> <
+        ProfileBio / >
+        <
+        Content style = {
+            { paddingLeft: '20%', paddingRight: '20%' } } >
+        <
+        Tabs defaultActiveKey = "1" >
+        <
+        TabPane tab = "Styles"
+        key = "1" >
+        <
+        ImageList / >
+        <
+        /TabPane> <
+        TabPane tab = "Reviews"
+        key = "2" >
+        <
+        ReviewList / >
+        <
+        /TabPane> <
+        TabPane tab = "Menu"
+        key = "3" >
+        <
+        MenuList / >
+        <
+        /TabPane> <
+        /Tabs> <
+        /Content> <
+        /Layout>
+    );
 }
 
 export default ProfileView;
