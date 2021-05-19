dd9b0cbed6910597e77de330ef23ade960a9306e
raphaelshim <raphaelshim@kaist.ac.kr>
2021-05-20 01:49:32 +0900

    Code aligned

--- src/components/SearchListItem.js
+++ src/components/SearchListItem.js
@@ -6,56 +6,53 @@ import '../css/SearchList.css';
 
 const { Title, Text } = Typography;
 
-const IconText = ({ icon, text }) => ( <
-    Space > { React.createElement(icon) } { text } <
-    /Space>
+const IconText = ({ icon, text }) => ( 
+  <Space > 
+    { React.createElement(icon) } 
+    { text } 
+  </Space>
 );
 
 export default class SearchListItem extends React.Component {
-    render() {
-        return ( <
-            Row className = 'search-list' >
-            <
-            Col span = { 18 } >
-            <
-            Space direction = 'vertical'
+  render() {
+    return ( 
+      <Row className = 'search-list' >
+        <Col span = { 18 } >
+          <Space direction = 'vertical'
             className = 'search-results' >
-            <
-            Title level = { 4 }
-            strong className = "stylist-name" > { this.props.name } < /Title>  <
-            Space direction = 'horizontal'
-            className = "profile-infos" >
-            <
-            IconText icon = { StarOutlined }
-            text = { this.props.rating }
-            />   <
-            Divider type = 'vertical' / >
-            <
-            IconText icon = { MessageOutlined }
-            text = { this.props.reviews }
-            />  <
-            Divider type = 'vertical' / >
-            <
-            IconText icon = { EyeOutlined }
-            text = { this.props.followers }
-            />< /
-            Space > <
-            Text type = 'primary'
-            className = "profile-intro" > { this.props.intro } < /Text> <
-            Text type = 'secondary'
-            className = "profile-salon"
-            strong > { this.props.salon } < /Text> < /
-            Space > < /
-            Col > <
-            Col span = { 2 }
-            /> <
-            Col span = { 4 } >
-            <
-            Image className = 'profile-image'
-            src = { this.props.image }
-            /> < /
-            Col > <
-            /Row>
-        );
-    }
+            <Title level = { 4 }
+              strong 
+              className = "stylist-name" > 
+              { this.props.name } 
+            </Title>  
+            <Space direction = 'horizontal'
+              className = "profile-infos" >
+              <IconText icon = { StarOutlined }
+                text = { this.props.rating } />   
+              <Divider type = 'vertical' />
+              <IconText icon = { MessageOutlined }
+                text = { this.props.reviews } />  
+              <Divider type = 'vertical' />
+              <IconText icon = { EyeOutlined }
+                text = { this.props.followers } />
+            </Space > 
+            <Text type = 'primary'
+              className = "profile-intro" > 
+              { this.props.intro } 
+            </Text> 
+            <Text type = 'secondary'
+              className = "profile-salon"
+              strong > 
+              { this.props.salon } 
+            </Text> 
+          </Space > 
+        </Col > 
+        <Col span = { 2 } /> 
+        <Col span = { 4 } >
+          <Image className = 'profile-image'
+            src = { this.props.image } /> 
+        </Col > 
+      </Row>
+    );
+  }
 }
